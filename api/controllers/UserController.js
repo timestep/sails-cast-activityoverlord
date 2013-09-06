528b7f05da00967669895ec1a406657e14c37d5d
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-06 13:00:29 -0400

    added edit and update method

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -50,6 +50,25 @@ module.exports = {
             usr: usr
         });
     });
- }
+ },
 
+ 'edit':function(req,res,next){
+    User.findOne(req.param('id'), function foundUser(err,user){
+        if(err) return next(err);
+        if(!user) return next();
+
+        res.view({
+            user: user
+        });
+    });
+ },
+
+ 'update': function(req,res,next){
+     User.update(req.param('id'), req.params.all(), function userUpdated(err){
+         if(err) {
+             return res.redirect('/user/edit/'+ req.param('id'));
+         }
+         res.redirect('/user/show/' + req.param('id'));
+     });
+ }
 };
