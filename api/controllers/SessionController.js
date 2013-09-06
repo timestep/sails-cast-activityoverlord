35925dbce7ba6ca6ab6d9a7990b395342007828d
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-06 15:20:40 -0400

    session

---
+++ api/controllers/SessionController.js
@@ -0,0 +1,19 @@
+/**
+ * SessionController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+  /* e.g.
+  sayHello: function (req, res) {
+    res.send('hello world!');
+  }
+  */
+ 'new': function(req, res){
+     res.view('session/new');
+ }
+
+};
