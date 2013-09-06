d35ebeb6c9a7922a616e7b97a246df9ffc45f54e
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-06 13:37:40 -0400

    attempting to add postgresql

--- config/adapters.js
+++ config/adapters.js
@@ -16,26 +16,26 @@ module.exports.adapters = {
 
   // If you leave the adapter config unspecified 
   // in a model definition, 'default' will be used.
-  'default': 'disk',
-
-  // In-memory adapter for DEVELOPMENT ONLY
-  memory: {
-    module: 'sails-memory'
-  },
-
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data IS preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
-
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  mysql: {
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
-  }
+//  'default': 'disk',
+//
+//  // In-memory adapter for DEVELOPMENT ONLY
+//  memory: {
+//    module: 'sails-memory'
+//  },
+//
+//  // Persistent adapter for DEVELOPMENT ONLY
+//  // (data IS preserved when the server shuts down)
+//  disk: {
+//    module: 'sails-disk'
+//  },
+//
+//  // MySQL is the world's most popular relational database.
+//  // Learn more: http://en.wikipedia.org/wiki/MySQL
+//  mysql: {
+//    module: 'sails-mysql',
+//    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+//    user: 'YOUR_MYSQL_USER',
+//    password: 'YOUR_MYSQL_PASSWORD',
+//    database: 'YOUR_MYSQL_DB'
+//  }
 };
