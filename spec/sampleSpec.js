47a662968f045b75bdffff0d43ca8cc5eece34fb
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-09 14:30:55 -0400

    attempting to add testing

---
+++ spec/sampleSpec.js
@@ -0,0 +1,11 @@
+var request = require('request');
+
+describe("Test Swuite", function(){
+	it("should respond with hello world", function(done) {
+	  request("http://localhost:1337/hello", function(error, response, body){
+	    expect(body).toEqual("hello world");
+	    done();
+	  });
+	});
+});
+
