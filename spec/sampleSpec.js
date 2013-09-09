a9bcad15aebc197c82961c78382e003f1ecd82b1
Kevin Sivabalan <jbosshellokitty@gmail.com>
2013-09-09 18:45:40 -0400

    removing redundant stuff

--- spec/sampleSpec.js
+++ spec/sampleSpec.js
@@ -3,7 +3,7 @@ var request = require('request');
 describe("Test Swuite", function(){
 	it("should respond with hello world", function(done) {
 	  request("http://localhost:1337/hello", function(error, response, body){
-	    expect(body).toEqual("hello world");
+	    expect(body).toContain("hello world");
 	    done();
 	  });
 	});
